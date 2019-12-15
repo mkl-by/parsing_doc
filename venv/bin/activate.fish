25048caa1048f3214545b537ee568ab311211c32
mkl-by <mkl-by@yandex.ru>
2019-12-15 21:55:44 +0300

    2 sohranjalka

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mkl/PycharmProjects/opis/venv"
+set -gx VIRTUAL_ENV "/home/mkl/PycharmProjects/parsing_doc/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
